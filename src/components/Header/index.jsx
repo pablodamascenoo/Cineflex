9552b79f68ffcb70dc89c6e6a55a244c62391b52
pablodamascenoo <pablofacex@gmail.com>
2022-03-23 17:14:04 -0300

    first commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,26 @@
+import styled from "styled-components"
+
+export default function Header(){
+
+    const Container = styled.header`
+    width: 100%;
+    height: 67px;
+    background-color: #C3CFD9;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    `
+
+    const Title = styled.p`
+    @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+    font-size: 34px;
+    color: #E8833A;
+    font-family: 'Roboto';
+    `
+
+    return(
+        <Container>
+            <Title>CINEFLEX</Title>
+        </Container>
+    )
+}
