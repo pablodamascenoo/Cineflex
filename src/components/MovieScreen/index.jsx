37ada8b227bafdc5a15584712a970b8f4c01efcc
pablodamascenoo <pablofacex@gmail.com>
2022-03-24 19:48:50 -0300

    feat: added axios get movies

--- src/components/MovieScreen/index.jsx
+++ src/components/MovieScreen/index.jsx
@@ -1,16 +1,30 @@
-import {Container, Title, BoxTitle, BoxMovies} from "./style"
+import {useState, useEffect} from "react"
+import axios from "axios"
+
 import Movie from "../Movie"
+
+import {Container, Title, BoxTitle, BoxMovies} from "./style"
 
 export default function MovieScreen(){
 
-    const array = [1,2,3,4,5]
+    const [movies, SetMovies] = useState([])
+
+    useEffect(()=>{
+        const promisse = axios.get("https://mock-api.driven.com.br/api/v5/cineflex/movies")
+        
+        promisse.then(obj=>{
+            const {data} = obj
+            SetMovies(data)
+    })
+        promisse.catch(err=>{ console.log(err.response.status)})
+    },[])
 
     return(
         <Container>
             <BoxTitle>
                 <Title>Selecione o Filme</Title>
             </BoxTitle>
-            <BoxMovies>{array.map(index=>{return <Movie key={index}/>})}</BoxMovies>
+            <BoxMovies>{movies.map(movie=>{return <Movie key={movie.id} image={movie.posterURL} id={movie.id}/> }) }</BoxMovies>
         </Container>
     )
 }
